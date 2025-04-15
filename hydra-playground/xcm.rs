b62ce1757d37f04ed2fd83c21c14ff4257297001
chungquantin <56880684+chungquantin@users.noreply.github.com>
2025-04-16 00:23:10 +0700

    chore: remove hydra-playground

--- hydra-playground/xcm.rs
+++
@@ -1,8 +0,0 @@
-use ink::xcm::{
-    prelude::{Asset, Junction::Parachain, Location, OriginKind, Weight, Xcm, XcmHash},
-    VersionedXcm,
-};
-
-// fn form_transact_on_sibling_para(dest: u32, encoded_call: Vec<u8>) -> VersionedXcm<()> {
-//
-// }
